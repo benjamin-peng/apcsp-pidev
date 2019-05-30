fbe6914f48308fa6d1e1c9910c5f09f0d73d1f78
pengb1823 <33039063+pengb1823@users.noreply.github.com>
2019-05-29 23:49:10 -0700

    Update student.h

--- cdemo/myproject/student.h
+++ cdemo/myproject/student.h
@@ -5,8 +5,4 @@ struct Student {
   int ID;
 };
 
-void printStudent(struct Student* student) {
-  printf("enter first and last name: %s %s\n", student->first, student->last);
-  printf("age: %d\n", student->age);
-  printf("ID: %d\n", student->ID);
-}
+void printStudent(struct Student* student);
